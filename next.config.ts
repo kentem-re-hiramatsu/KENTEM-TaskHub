18b413c804cf28493f49a8ffaaeda32ae57d7ee1
re-hiramatsu <re-hiramatsu@kentem.co.jp>
2026-05-08 20:46:36 +0900

    chore: Next.js + TypeScript 環境を追加

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: false,
+  output: 'standalone',
+  reactCompiler: true,
+};
+
+export default nextConfig;
